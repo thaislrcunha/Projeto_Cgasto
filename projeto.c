49f2e328f9c93b45721fce0d296a6215766b94c9
Thais Cunha <thaiscunha@Thaiss-MacBook-Pro.local>
2025-06-13 15:32:41 -0300

    first commit

---
+++ projeto.c
@@ -0,0 +1,8 @@
+/* sistema digital de controle financeiro pessoal, 
+com funcionalidades evolutivas que permitam registrar, 
+organizar, visualizar e analisar receitas e despesas*/
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
